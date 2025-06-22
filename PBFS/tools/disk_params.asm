4e2b582722fbafff7b3473c6af542bec00061e9e
Jyoti Mankotia <chanderjyoti.mankotia@nice.com>
2025-06-22 22:08:24 +0530

    Finished PBFS Prototype FS 1

---
+++ PBFS/tools/disk_params.asm
@@ -0,0 +1,21 @@
+[BITS 16]
+[GLOBAL get_drive_params_real_asm]
+
+get_drive_params_real_asm:
+    push es
+    xor ax, ax
+    mov ax, es
+
+    mov ah, 0x48 ; Func
+    ; Drive number already in dl
+    ; Buffer already in si
+    int 0x13
+    jc .error
+
+    xor ax, ax
+    pop es
+    ret
+.error:
+    mov ax, 1 ; Error code
+    pop es
+    ret
