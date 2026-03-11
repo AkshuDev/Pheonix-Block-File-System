19ed0e2ecaa404f21b81a6c9109ed62f2fd005bb
AkshuDev <akshobhyasasun@gmail.com>
2026-03-11 22:39:09 +0530

    Small fix again

--- PBFS/PEFI/src/efi_disk.c
+++ PBFS/PEFI/src/efi_disk.c
@@ -1,6 +1,8 @@
 #include <pefi_types.h>
 #include <pefi_disk.h>
 
+LIB EFI_GUID EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID = { 0x0964e5b22, 0x6459, 0x11d2, { 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b } };
+
 LIB EFI_BLOCK_IO_PROTOCOL* pefi_find_block_io(EFI_SYSTEM_TABLE* SystemTable) {
 	EFI_STATUS status;
 	EFI_HANDLE *handles;
