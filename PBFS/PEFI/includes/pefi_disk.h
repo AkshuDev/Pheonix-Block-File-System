c9f1f9ac9cef61103510908153304ac3df663d0a
AkshuDev <akshobhyasasun@gmail.com>
2026-03-11 21:21:27 +0530

    Small PEFI fix

--- PBFS/PEFI/includes/pefi_disk.h
+++ PBFS/PEFI/includes/pefi_disk.h
@@ -6,8 +6,19 @@
 #include "pefi_types.h"
 #include "pefi.h"
 
-// Now for forward declaring structs
-// efi_disk
+struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL{
+    uint64_t Revision;
+    EFI_STATUS (EFIAPI *OpenVolume)(void* This, void** Root);
+};
+
+struct EFI_FILE_PROTOCOL{
+    uint64_t Revision;
+    EFI_STATUS (EFIAPI *Open)(void* This, void** NewHandle, uint16_t* FileName, uint64_t OpenMode, uint64_t Attributes);
+    EFI_STATUS (EFIAPI *Read)(void* This, uint64_t* BufferSize, void* Buffer);
+};
+
+EFI_GUID EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID = { 0x0964e5b22, 0x6459, 0x11d2, { 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b } };
+
 typedef struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL EFI_SIMPLE_FILE_SYSTEM_PROTOCOL;
 typedef struct EFI_FILE_PROTOCOL EFI_FILE_PROTOCOL;
 
--- PBFS/PEFI/src/efi_disk.c
+++ PBFS/PEFI/src/efi_disk.c
@@ -1,19 +1,6 @@
 #include <pefi_types.h>
 #include <pefi_disk.h>
 
-struct EFI_SIMPLE_FILE_SYSTEM_PROTOCOL{
-    uint64_t Revision;
-    EFI_STATUS (EFIAPI *OpenVolume)(void* This, void** Root);
-};
-
-struct EFI_FILE_PROTOCOL{
-    uint64_t Revision;
-    EFI_STATUS (EFIAPI *Open)(void* This, void** NewHandle, uint16_t* FileName, uint64_t OpenMode, uint64_t Attributes);
-    EFI_STATUS (EFIAPI *Read)(void* This, uint64_t* BufferSize, void* Buffer);
-};
-
-LIB EFI_GUID EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID = { 0x0964e5b22, 0x6459, 0x11d2, { 0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b } };
-
 LIB EFI_BLOCK_IO_PROTOCOL* pefi_find_block_io(EFI_SYSTEM_TABLE* SystemTable) {
 	EFI_STATUS status;
 	EFI_HANDLE *handles;
